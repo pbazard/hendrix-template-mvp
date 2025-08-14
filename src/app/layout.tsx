cc83cd0a1f18f411a99fd5825c54a753517b1f2a
Philippe Bazard <bazard.philippe@gmail.com>
2025-08-14 21:28:41 +0000

    feat: update ConfigureAmplify component and adjust tsconfig paths for improved module resolution

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <ConfigureAmplifyClientSide />
       <body className={inter.className}>
         {children}
         <Toaster richColors position="top-right" />
